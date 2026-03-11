d98e079aec8f76d48454a79e55a7829ff5265f79
Assignment Submitter <student@assignments.local>
2026-03-11 18:46:04 +0530

    Update 4B

---
+++ 4B/Q8.c
@@ -0,0 +1,12 @@
+/*
+    PRN : 260240130002 , 260240130028
+  Program for number conversions (decimal, binary, octal, hexadecimal).
+*/
+#include<stdio.h>
+
+int main(void)
+{
+	int = 
+}
+
+int DectoBin(
